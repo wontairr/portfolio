6da871e8c94fff721bbd69f7ac645322a78cbd9f
Wontairr <wontairr@proton.me>
2026-06-16 11:02:33 -0500

    Further work on portfolio section.

---
+++ assets/scripts/index.js
@@ -0,0 +1,15 @@
+const aboutSection = document.getElementById("about-section");
+
+// FIXME: Doesn't always work at low res.
+function resizeAboutSection()
+{
+    const newHeight = Math.max(aboutSection.clientHeight,window.innerHeight).toString() + "px";
+    aboutSection.style.height = newHeight;
+}
+
+if (document.readyState === "loading") {
+    // Make the about section take up the whole screen height so that the portfolio isn't peeking in visually.
+    document.addEventListener("DOMContentLoaded",resizeAboutSection);
+} else {
+    resizeAboutSection();
+}
